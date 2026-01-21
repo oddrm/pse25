e60dc2c90789b725033f19c001c43eaa4e635b81
mariiapohasii <mariapogasiy@gmail.com>
2026-01-21 09:55:36 +0200

    Add EntryInfo component and connect it to index

---
+++ frontend/app/components/entryInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-full relative">
+    <!-- Title -->
+    <div class="absolute top-4 left-1/2 -translate-x-1/2 text-lg font-semibold">
+      INFORMATION
+    </div>
+
+    <!-- Тут пізніше може бути контент -->
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
