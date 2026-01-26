a8016dd4607dd703d8624cbc036e59dbc46b727b
Daniel Meiborg <mail@danielmeiborg.com>
2026-01-26 11:00:14 +0100

    test: improve local build script

--- run.test.linux.sh
+++ run.test.linux.sh
@@ -1,3 +1,5 @@
-cd backend && cargo test --no-run && cd ..
+# Build locally
+cd backend && RUSTFLAGS="-A warnings" cargo test --no-run && cd ..
 
+# Run in container
 sudo docker compose -f compose.test.yaml up --no-attach db --remove-orphans
