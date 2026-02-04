719fe49cadb6e225515a41bfe3259ff0335cd3f2
Deniz Ilicali <denizilicali0@gmail.com>
2026-02-04 01:17:51 +0100

    adding DB tables

--- backend/src/storage/models.rs
+++ backend/src/storage/models.rs
@@ -88,3 +88,119 @@ pub struct Topic {
     pub serialization_format: Option<String>,
     pub created_at: DateTime<Utc>,
 }
+
+#[derive(Queryable, Selectable, Insertable, Debug, Clone, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::metadata_info)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[serde(crate = "rocket::serde")]
+pub struct MetadataInfo {
+    pub id: i64,
+    pub metadata_id: i64,
+    pub data_spec_version: Option<String>,
+    pub dataset_license: Option<String>,
+    pub meta_data_spec_version: Option<String>,
+    pub sequence_version: Option<String>,
+    pub software_info: Option<String>,
+    pub software_version: Option<String>,
+    pub time_human: Option<String>,
+    pub time_machine: Option<f64>,
+    pub created_at: DateTime<Utc>,
+}
+
+
+#[derive(Queryable, Selectable, Insertable, Debug, Clone, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::metadata_labeling)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[serde(crate = "rocket::serde")]
+pub struct MetadataLabeling {
+    pub metadata_id: i64,
+    pub labeling_key: String,
+    pub creation_time_human: Option<String>,
+    pub freetext: Option<String>,
+    pub policy_version: Option<String>,
+    pub provider: Option<String>,
+    pub sensors_json: Option<serde_json::Value>,
+}
+
+#[derive(Queryable, Selectable, Insertable, Debug, Clone, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::metadata_scenario)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[serde(crate = "rocket::serde")]
+pub struct MetadataScenario {
+    pub id: i64,
+    pub metadata_id: i64,
+    pub environment_dynamics: Option<String>,
+    pub environment_tags: Option<serde_json::Value>,
+    pub name: Option<String>,
+}
+
+
+#[derive(Queryable, Selectable, Insertable, Debug, Clone, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::metadata_dataset_sequence)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[serde(crate = "rocket::serde")]
+pub struct MetadataDatasetSequence {
+    pub id: i64,
+    pub metadata_id: i64,
+    pub description: Option<String>,
+    pub distance: Option<f64>,
+    pub duration: Option<f64>,
+    pub lat_starting_point_deg: Option<f64>,
+    pub lon_starting_point_deg: Option<f64>,
+    pub name: Option<String>,
+    pub weather: Option<serde_json::Value>,
+    pub created_at: DateTime<Utc>,
+}
+
+
+#[derive(Queryable, Selectable, Insertable, Debug, Clone, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::metadata_setup)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[serde(crate = "rocket::serde")]
+pub struct MetadataSetup {
+    pub id: i64,
+    pub metadata_id: i64,
+    pub name: Option<String>,
+    pub platform_description_link: Option<String>,
+    pub created_at: DateTime<Utc>,
+}
+
+
+#[derive(Queryable, Selectable, Insertable, Debug, Clone, Serialize, Deserialize)]
+#[diesel(table_name = crate::schema::metadata_sensor)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[serde(crate = "rocket::serde")]
+pub struct MetadataSensor {
+    pub id: i64,
+    pub metadata_id: i64,
+    pub sensor_key: String,
+    pub acquisition_rate: Option<f64>,
+    pub acquistion_mode: Option<String>,
+    pub capture_rate: Option<f64>,
+    pub channel_number: Option<i32>,
+    pub channel_space: Option<String>,
+    pub firmware_version: Option<String>,
+    pub focus_position_m: Option<f64>,
+    pub fov_horizontal_deg: Option<f64>,
+    pub fov_vertical_deg: Option<f64>,
+    pub frame_id: Option<String>,
+    pub freetext: Option<String>,
+    pub image_height: Option<i32>,
+    pub image_width: Option<i32>,
+    pub lens: Option<String>,
+    pub manufacturer: Option<String>,
+    pub max_exposure: Option<i32>,
+    pub model: Option<String>,
+    pub mtu: Option<i32>,
+    pub optical_center_frame: Option<String>,
+    pub ros_topics: Option<serde_json::Value>,
+    pub sw_trigger_rate: Option<f64>,
+    pub time_stamp_accuracy: Option<String>,
+    pub time_sync_method: Option<String>,
+    pub trigger_method: Option<String>,
+    pub trigger_mode: Option<bool>,
+    pub trigger_reference: Option<String>,
+    pub trigger_source: Option<String>,
+    pub type_: Option<String>,
+    pub created_at: DateTime<Utc>,
+}
