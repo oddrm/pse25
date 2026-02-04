c9b09d56a791255fdf18d041cd64f335f909572e
Deniz Ilicali <denizilicali0@gmail.com>
2026-02-04 01:26:00 +0100

    changes

---
+++ backend/migrations/2026-01-27-120000_add_metadata_definition_tables/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS metadata_sensor_ros_topic;
+DROP TABLE IF EXISTS metadata_sensor;
+DROP TABLE IF EXISTS metadata_setup;
+DROP TABLE IF EXISTS metadata_dataset_sequence;
+DROP TABLE IF EXISTS metadata_scenario;
+DROP TABLE IF EXISTS metadata_labeling;
+DROP TABLE IF EXISTS metadata_info;
--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -67,9 +67,117 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    metadata_info (id) {
+        id -> BigInt,
+        metadata_id -> BigInt,
+        data_spec_version -> Nullable<Varchar>,
+        dataset_license -> Nullable<Varchar>,
+        meta_data_spec_version -> Nullable<Varchar>,
+        sequence_version -> Nullable<Varchar>,
+        software_info -> Nullable<Varchar>,
+        software_version -> Nullable<Varchar>,
+        time_human -> Nullable<Varchar>,
+        time_machine -> Nullable<Double>,
+        created_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    metadata_labeling (metadata_id, labeling_key) {
+        metadata_id -> BigInt,
+        labeling_key -> Varchar,
+        creation_time_human -> Nullable<Varchar>,
+        freetext -> Nullable<Text>,
+        policy_version -> Nullable<Varchar>,
+        provider -> Nullable<Varchar>,
+        sensors_json -> Nullable<Jsonb>,
+    }
+}
+
+diesel::table! {
+    metadata_scenario (id) {
+        id -> BigInt,
+        metadata_id -> BigInt,
+        environment_dynamics -> Nullable<Varchar>,
+        environment_tags -> Nullable<Jsonb>,
+        name -> Nullable<Varchar>,
+    }
+}
+
+diesel::table! {
+    metadata_dataset_sequence (id) {
+        id -> BigInt,
+        metadata_id -> BigInt,
+        description -> Nullable<Text>,
+        distance -> Nullable<Double>,
+        duration -> Nullable<Double>,
+        lat_starting_point_deg -> Nullable<Double>,
+        lon_starting_point_deg -> Nullable<Double>,
+        name -> Nullable<Varchar>,
+        weather -> Nullable<Jsonb>,
+        created_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    metadata_setup (id) {
+        id -> BigInt,
+        metadata_id -> BigInt,
+        name -> Nullable<Varchar>,
+        platform_description_link -> Nullable<Varchar>,
+        created_at -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    metadata_sensor (id) {
+        id -> BigInt,
+        metadata_id -> BigInt,
+        sensor_key -> Varchar,
+        acquisition_rate -> Nullable<Double>,
+        acquistion_mode -> Nullable<Varchar>,
+        capture_rate -> Nullable<Double>,
+        channel_number -> Nullable<Integer>,
+        channel_space -> Nullable<Varchar>,
+        firmware_version -> Nullable<Varchar>,
+        focus_position_m -> Nullable<Double>,
+        fov_horizontal_deg -> Nullable<Double>,
+        fov_vertical_deg -> Nullable<Double>,
+        frame_id -> Nullable<Varchar>,
+        freetext -> Nullable<Text>,
+        image_height -> Nullable<Integer>,
+        image_width -> Nullable<Integer>,
+        lens -> Nullable<Varchar>,
+        manufacturer -> Nullable<Varchar>,
+        max_exposure -> Nullable<Integer>,
+        model -> Nullable<Varchar>,
+        mtu -> Nullable<Integer>,
+        optical_center_frame -> Nullable<Varchar>,
+        ros_topics -> Nullable<Jsonb>,
+        sw_trigger_rate -> Nullable<Double>,
+        time_stamp_accuracy -> Nullable<Varchar>,
+        time_sync_method -> Nullable<Varchar>,
+        trigger_method -> Nullable<Varchar>,
+        trigger_mode -> Nullable<Bool>,
+        trigger_reference -> Nullable<Varchar>,
+        trigger_source -> Nullable<Varchar>,
+        type_ -> Nullable<Varchar>,
+        created_at -> Timestamptz,
+    }
+}
+
+
 diesel::joinable!(tags -> entries (entry_id));
 diesel::joinable!(metadata -> entries (entry_id));
 diesel::joinable!(sequences -> entries (entry_id));
 diesel::joinable!(topics -> entries (entry_id));
+diesel::joinable!(metadata_info -> metadata (metadata_id));
+diesel::joinable!(metadata_labeling -> metadata (metadata_id));
+diesel::joinable!(metadata_scenario -> metadata (metadata_id));
+diesel::joinable!(metadata_dataset_sequence -> metadata (metadata_id));
+diesel::joinable!(metadata_setup -> metadata (metadata_id));
+diesel::joinable!(metadata_sensor -> metadata (metadata_id));
 
-diesel::allow_tables_to_appear_in_same_query!(entries, metadata, sequences, tags, topics,);
+
+diesel::allow_tables_to_appear_in_same_query!(entries, metadata, sequences, tags, topics, metadata_info, metadata_labeling, metadata_scenario, metadata_dataset_sequence, metadata_setup, metadata_sensor);
