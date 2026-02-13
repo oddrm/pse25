b863bcaebf9af432225b7037a4eddd965bd829ff
Olivia <udnps@student.kit.edu>
2026-02-13 15:12:09 +0100

    fix: Richtige daisy ui Farben verwendet und die anzeige für plugins auf einzelen dateien auf die main page verschoben

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,43 +1,66 @@
 <template>
   <div class="relative w-full h-screen">
 
-    <!-- Main -->
+    <!--  MAIN  -->
     <div class="transition-all duration-300" :class="infoOpen ? 'w-2/3' : 'w-full'">
-      <!-- Table -->
+      
+      <!-- ---------- Plugin Status Header ---------- -->
+      <PluginStatusHeader />
+
+      <!-- ---------- Table / Entry Selection ---------- -->
       <Table @select="openInfo" />
     </div>
-    <!-- Right info curtain -->
-    <div class="fixed top-0 right-0 h-full transition-all duration-300 bg-base-200 pt-12"
-      :class="infoOpen ? 'w-1/3' : 'w-6'">
-      <!-- Open -->
+
+
+    <!--RIGHT INFO CURTAIN -->
+    <div
+      class="fixed top-0 right-0 h-full transition-all duration-300 bg-base-200 pt-12"
+      :class="infoOpen ? 'w-1/3' : 'w-6'"
+    >
+
+      <!-- ---------- Info Curtain OPEN ---------- -->
       <div v-if="infoOpen" class="h-full relative cursor-pointer">
-        <button @click="infoOpen = false" class="absolute left-[-0.25rem] top-1/2 -translate-y-1/2 z-50">
+
+        <!-- Close Button -->
+        <button
+          @click="infoOpen = false"
+          class="absolute left-[-0.25rem] top-1/2 -translate-y-1/2 z-50"
+        >
           <Icon icon="octicon:triangle-right" class="w-10 h-10 text-blue-800" />
         </button>
+
+        <!-- Entry Details -->
         <EntryInfo :entryID="selectedEntryID" />
       </div>
 
 
-      <!-- Closed -->
-      <div v-else @click="infoOpen = true" class="absolute left-[-2.0rem] top-1/2 -translate-y-1/2 pt-12">
+      <!-- ---------- Info Curtain CLOSED ---------- -->
+      <div
+        v-else
+        @click="infoOpen = true"
+        class="absolute left-[-2.0rem] top-1/2 -translate-y-1/2 pt-12"
+      >
         <Icon icon="octicon:triangle-left" class="w-10 h-10 text-blue-800" />
       </div>
-    </div>
 
+    </div>
   </div>
 </template>
 
-
 <script setup lang="ts">
+
+import { ref } from 'vue'
 import { Icon } from '@iconify/vue'
 import Table from '~/components/table.vue'
 import EntryInfo from '~/components/entryInfo.vue'
 const infoOpen = ref(false)
+
+// Currently selected table entry
 const selectedEntryID = ref<number | null>(null)
+
 
 const openInfo = (id: number) => {
   selectedEntryID.value = id
   infoOpen.value = true
 }
-
 </script>
